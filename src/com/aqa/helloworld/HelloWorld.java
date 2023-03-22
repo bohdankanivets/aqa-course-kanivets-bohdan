22196b172869b535935ba4884174185218e86011
Bohdan Kanivets <bohdan.kanivets@airslate.com>
2023-03-22 23:41:40 +0200

    Third commit

--- src/com/aqa/helloworld/HelloWorld.java
+++ src/com/aqa/helloworld/HelloWorld.java
@@ -3,5 +3,7 @@ package com.aqa.helloworld;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        System.out.println("Third commit in main");
     }
 }
