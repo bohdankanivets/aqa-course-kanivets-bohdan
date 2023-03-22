0bb98cdc29a09a0a90b5a113292755974b68af3f
Bohdan Kanivets <bohdan.kanivets@airslate.com>
2023-03-22 23:04:06 +0200

    Added println "First commit"

--- src/com/aqa/helloworld/HelloWorld.java
+++ src/com/aqa/helloworld/HelloWorld.java
@@ -4,6 +4,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
 
+        System.out.println("First commit");
+
         System.out.println("Third commit in main");
     }
 }
