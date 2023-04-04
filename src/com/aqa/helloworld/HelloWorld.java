ee4a19c4a2daa9f2fcf8d30da790288e0108fe3d
Bohdan Kanivets <bohdan.kanivets@airslate.com>
2023-04-04 22:32:02 +0300

    Initialized all types and printed them

--- src/com/aqa/helloworld/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package com.aqa.helloworld;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-
-        System.out.println("Third commit in main");
-    }
-}
